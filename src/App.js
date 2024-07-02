656eeba4a493d1a25f10eab4058507aa034db43f
Aayush-Sharma25 <94744334+Aayush-Sharma25@users.noreply.github.com>
2024-07-02 15:12:29 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'; // Import your CSS file here
+import CodeEditor from './Components/CodeEditor';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Simple Code Editor</h1>
+      <CodeEditor />
+    </div>
+  );
+}
+
+export default App;
